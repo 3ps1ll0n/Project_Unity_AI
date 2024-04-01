f876400f56a6c5c4f6cd9eaa521aeccd2ca20ba0
Elisabeth <elisabeth.huot@hotmail.ca>
2024-04-01 16:46:21 -0400

    Sauvegarde et download presque terminé
    Il y a juste un bug dans le téléchargement du niveau. Le bug vient du script sauvegardeNivieau dans la méthode créer assets, je trouverai le problème cette semaine

--- Assets/Scripts/NiveauManager.cs
+++ Assets/Scripts/NiveauManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using LootLocker.Requests;
+using UnityEngine.Networking;
 
 public class NiveauManager : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class NiveauManager : MonoBehaviour
     private string nomNiveau; //Pour le sauvegarder
     public GameObject niveauUploadUI; //Pour visuellement voir le InputField
 
+    public GameObject entreeNiveauItem;
+    public Transform contenuDonneeNiveau;
     public void creerNiveau() //Sauvegarder un niveau
     {
 
@@ -75,6 +78,37 @@ public class NiveauManager : MonoBehaviour
                 Debug.Log("erreur upload candidat asset");
             }
         });
+    }
+
+    public void telechargerDonneeNiveau()
+    {
+        LootLockerSDKManager.GetAssetListWithCount(10, (reponse) =>
+        {
+            for(int i = 0; i< reponse.assets.Length; i++)
+            {
+                GameObject afficherItem = Instantiate(entreeNiveauItem, transform.position, Quaternion.identity);
+                afficherItem.transform.SetParent(contenuDonneeNiveau);
+
+                afficherItem.GetComponent<DonneeEntreeNiveau>().identification = i;
+                afficherItem.GetComponent<DonneeEntreeNiveau>().nomNiveau = reponse.assets[i].name;
+
+                LootLockerFile[] fichiersImageNiveau = reponse.assets[i].files;
+                StartCoroutine(chargerImageNiveau(fichiersImageNiveau[0].url.ToString(), afficherItem.GetComponent<DonneeEntreeNiveau>().imageNiveau));
+
+                afficherItem.GetComponent<DonneeEntreeNiveau>().dossierTexteURL = fichiersImageNiveau[1].url.ToString();
+            
+            }
+        }, null, true);
+    }
+
+    IEnumerator chargerImageNiveau(string imageURL, Image imageNiveau)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(imageURL);
+        yield return www.SendWebRequest();
+
+        Texture2D imageChargee = DownloadHandlerTexture.GetContent(www);
+        imageNiveau.sprite = Sprite.Create(imageChargee, new Rect(0.0f, 0.0f, imageChargee.width, imageChargee.height), Vector2.zero);
+
     }
 
 }
--- Assets/Scripts/SauvegardeNiveau.cs
+++ Assets/Scripts/SauvegardeNiveau.cs
@@ -11,6 +11,9 @@ public class SauvegardeNiveau : MonoBehaviour
     [SerializeField] private string[] nomsAssets;
     [SerializeField] private Vector3[] positionsAssets;
 
+    public GameObject[] objetsPossibles;
+
+
     public void trouverAssetsSauvegarde()
     {
         if(assetsAsauvegarder != null)
@@ -44,6 +47,64 @@ public class SauvegardeNiveau : MonoBehaviour
             ecrire.WriteLine(info);
         }
         ecrire.Close();
+    }
+
+    public void chargerNiveau()
+    {
+        foreach(GameObject sauveugardableObject in GameObject.FindGameObjectsWithTag("Sauvegardable"))
+        {
+            Destroy(sauveugardableObject);
+        }
+        string referenceFichier = "Assets/Screenshots/Niveau-Donnee.txt";
+        StreamReader lecteur = new StreamReader(referenceFichier);
+
+        int nombreLignes = 0;
+        while(lecteur.ReadLine() != null) { nombreLignes++; }
+
+        nomsAssets = new string[0];
+        positionsAssets = new Vector3[0];
+
+        nomsAssets = new string[nombreLignes];
+        positionsAssets = new Vector3[nombreLignes];
+
+        lecteur.Close();
+
+        StreamReader lecteur2 = new StreamReader(referenceFichier);
+
+        while (!lecteur2.EndOfStream)
+        {
+            for (int i = 0; i < nombreLignes; i++)
+            {
+                string[] donnee = lecteur2.ReadLine().Split(',');
+                nomsAssets[i] = donnee[0];
+                positionsAssets[i].x = float.Parse(donnee[1]);
+                positionsAssets[i].y = float.Parse(donnee[2]);
+                positionsAssets[i].z = float.Parse(donnee[3]);
+            }
+        }
+        lecteur2.Close();
+        
+        creerAsset();
+
+        
+    }
+    public void creerAsset()
+    {
+        for(int i = 0;i<nomsAssets.Length;i++)
+        {
+            for(int j = 0;j<objetsPossibles.Length;j++)
+            {
+                if (objetsPossibles[j].name == nomsAssets[i])
+                {
+                    Instantiate(objetsPossibles[j], positionsAssets[i],Quaternion.identity);
+
+                }
+                else
+                {
+                    Debug.Log("putain");
+                }
+            }
+        }
     }
     
 }
